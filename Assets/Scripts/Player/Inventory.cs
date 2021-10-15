9d18d575d391d8b52146e9e5783a8dd167ed5693
Chun Deng <dengchun@berkeley.edu>
2021-10-14 21:52:09 -0700

    Inventory

--- Assets/Scripts/Player/Inventory.cs
+++ Assets/Scripts/Player/Inventory.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    [System.Serializable]
+    private class InventorySerialObject {
+        public List<(string, int)> itemList;
+    }
+    public static void add_to_inventory(string item) {
+        InventorySerialObject deserial = JsonUtility.FromJson<InventorySerialObject>(Application.dataPath + Path.PathSeparator + "inventory.json");
+        List<(string, int)> items = deserial.itemList;
+        if (items.Count == 0) {
+            items.Add((item, 1));
+        }
+        foreach ((string i, int j) in items) {
+            if (item == i) {
+                int store = j + 1;
+                items.Remove((i, j));
+                items.Add((i, store));
+                break;
+            }
+        }
+        string data = JsonUtility.ToJson(deserial);
+        using(StreamWriter writer = new StreamWriter(Application.dataPath + Path.PathSeparator + "inventory.json")) {
+            writer.Write(data);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
