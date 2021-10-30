49228a82a55f6a00bd3f04b77e6590cc6715d9f2
Matthew Sun <matthewsunsailing@berkeley.edu>
2021-10-29 18:46:13 -0700

    item fixes

--- Assets/Scripts/NPC/ItemController.cs
+++ Assets/Scripts/NPC/ItemController.cs
@@ -102,7 +102,7 @@ public class ItemController : MonoBehaviour
     public void ChangeState() {
         m_DiaBox.SetActive(true);
         p_ObjectName = this.gameObject.name;
-        Inventory.add_to_inventory(this.gameObject.name, 1);
+        // Inventory.add_to_inventory(this.gameObject.name, 1);
     }
     #endregion
 
