08a88a479b3988222d92dcd03d1c974ac9c4afbe
Matthew Sun <matthewsunsailing@berkeley.edu>
2021-12-03 18:57:04 -0800

    bruh

--- Assets/Scripts/Scene/SceneSwitch.cs
+++ Assets/Scripts/Scene/SceneSwitch.cs
@@ -8,40 +8,35 @@ public class SceneSwitch : MonoBehaviour
     #region Editor Variables
     [SerializeField]
     [Tooltip("The name of the scene to switch to.")]
-    private string name;
+    private string m_SceneName;
 
-    [SerializeField]
-    [Tooltip("the item to consume")]
-    private string item;
+    // [SerializeField]
+    // [Tooltip("the item to consume")]
+    // private string item;
 
-    [SerializeField]
-    [Tooltip("switch scenes without consuming item")]
-    private bool m_Switch;
-    #endregion
+    // [SerializeField]
+    // [Tooltip("The inventory panel")]
+    // private GameObject inv_Panel;
 
-    #region Cached References
-    private Inventory inv_Panel;
-    #endregion
-
-    #region Initialization Methods
-    public void Start() {
-        inv_Panel = Inventory.inv;
-    }
+    // [SerializeField]
+    // [Tooltip("switch scenes without consuming item")]
+    // private bool m_Switch;
     #endregion
 
     #region Scene Methods
     public void Switch() {
-        if (m_Switch){
-            inv_Panel.GetComponent<PopulateInventory>().populate(); //reorganize the inventory
-            SceneManager.LoadScene(name);
-        } else if (Inventory.inv.get_Inventory().Contains(item)) {
-            Inventory.inv.remove_from_inventory(item); //update db
-            inv_Panel.GetComponent<PopulateInventory>().populate(); //reorganize the inventory
-            SceneManager.LoadScene(name);
-        }
-        else {
-            Debug.Log("no such item and m_switch is off");
-        }
+        // if (m_Switch){
+        //     inv_Panel.GetComponent<PopulateInventory>().populate(); //reorganize the inventory
+        //     SceneManager.LoadScene(name);
+        // } else if (Inventory.inv.get_Inventory().Contains(item)) {
+        //     Inventory.inv.remove_from_inventory(item); //update db
+        //     inv_Panel.GetComponent<PopulateInventory>().populate(); //reorganize the inventory
+        //     SceneManager.LoadScene(name);
+        // }
+        // else {
+        //     Debug.Log("no such item and m_switch is off");
+        // }
+        SceneManager.LoadScene(m_SceneName);
     }
     #endregion
 }
